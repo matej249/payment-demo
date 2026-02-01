57e77e270e2c4d6a34e2c3486815ab7cdb3d3112
matej249 <rmatej.sluzb@gmail.com>
2026-02-01 20:11:23 +0100

    feat: add controller with post create payment endpoint

---
+++ be/payment-demo/src/main/java/com/example/payment_demo/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.example.payment_demo.controller;
+
+import com.example.payment_demo.dto.PaymentRequestDTO;
+import com.example.payment_demo.dto.PaymentResponseDTO;
+import com.example.payment_demo.service.PaymentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payments")
+// ensure react app can call apis
+@CrossOrigin(origins = "http://localhost:3000")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/process")
+    public ResponseEntity<PaymentResponseDTO> createPayment(@Valid @RequestBody PaymentRequestDTO paymentRequestDTO) {
+        PaymentResponseDTO paymentResponse = paymentService.processPayment(paymentRequestDTO);
+
+        return new ResponseEntity<>(paymentResponse, HttpStatus.CREATED);
+    }
+}
