10b02b2a987bc88f1bf33238c91e50afa96015fb
matej249 <rmatej.sluzb@gmail.com>
2026-02-24 21:44:56 +0100

    feature: create User entity and its repository

---
+++ be/payment-demo/src/main/java/com/example/payment_demo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.payment_demo.repository;
+
+import com.example.payment_demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
